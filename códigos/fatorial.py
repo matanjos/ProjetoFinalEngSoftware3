b3c7e5dd6c6f8a947fbf8cb71ed18bf1ac0bcccd
ERONILDOJUNIOR <81919227+ERONILDOJUNIOR@users.noreply.github.com>
2021-09-28 11:05:56 -0300

    Update fatorial.py

--- códigos/fatorial.py
+++ códigos/fatorial.py
@@ -1,5 +1,5 @@
-def fatorial (n):
-	if n==1:
-		return 1
-	else:
-		return (n*fatorial(n-1))
+def fatorial (n): 				#Definição da função FATORIAL recebendo um valor "n" para ser calculado.
+	if n==1: 				#Critério de parada.
+		return 1			#
+	else:					#Recursividade.
+		return (n*fatorial(n-1))	
