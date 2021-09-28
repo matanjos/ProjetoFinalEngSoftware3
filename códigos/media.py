3c1fa1b18143b7719196310028aba32c9a7840b0
ezequias21 <56970805+ezequias21@users.noreply.github.com>
2021-09-28 12:02:49 -0300

    Update media.py
    Foi acrescentado duas funções: maiorNotaTurma(list) e calcMediaTurma(list)

--- códigos/media.py
+++ códigos/media.py
@@ -1,3 +1,20 @@
-  lista = [ 1, 2, 3, 4, 5] #cria uma lista com 5 elementos
-  media = sum(lista)/len(lista) #calcula a media dos elementos da lista  e salva numa variável chamada media
+  lista = [ 5.5, 2, 7, 9, 8.9] #cria uma lista contendo as medias de 5 estudantes
+  
+  
+  def maiorNotaTurma(lista):    #define uma função para calcular a maior nota da turma
+    maiorNota = 0
+    for nota in lista:
+       if nota > maiorNota:
+          maiorNota = nota
+    return maiorNota
+   
+  def calcMediaTurma(lista):  #define uma função para calcular a media da turma
+    mediaDaTurma = sum(lista)/len(lista) #calcula a media dos elementos da lista  e salva numa variável chamada media
+    return mediaDaTurma
+  
+  maiorNota = maiorNotaTurma(lista)
+  media = mediaDaTurma(lista)
+  
+  print(maiorNota) #exibe o valor contido na variavel maiorNota
   print(media) #exibe o valor contido na variavel media
+  
