3bf6ccf008a36ea7db9adfd7e37e7cc45ceaf459
matanjos <84453569+matanjos@users.noreply.github.com>
2021-09-30 15:00:22 -0300

    Create media.py

---
+++ códigos/media.py
@@ -0,0 +1,4 @@
+n1 = 9 # declaração e atribuição da variável n1
+n2 = 8 # declaração e atribuição da variável n2
+media = n1 # declaração e calculo da media das variáveis n1 e n2
+print(media) # exibição do valor calculado
